bbf60246832a6d78f0351272f7abeea2ec7194ef
pingreen <inductionforge@gmail.com>
2017-12-13 00:33:41 +0300

    deploy_test

--- bot.py
+++ bot.py
@@ -32,37 +32,45 @@ def log(message, answer):
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     usermarkup = telebot.types.ReplyKeyboardMarkup(True, False)
-    usermarkup.row('/start')
-    usermarkup.row('picnic', 'snickers')
-    usermarkup.row('Tofee', 'bounty')
+    usermarkup.row('Picnic', 'Rafaello')
+    usermarkup.row('Alpen Gold Funduk', 'Dark')
+    usermarkup.row('Alpen Gold', 'KitKat')
     usermarkup.row('заказ')
     bot.send_message(message.from_user.id, "You're welcome", reply_markup=usermarkup)
 
 #Ответы на вопросы и сказочки)
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    if message.text == 'picnic' or message.text == 'пикник':
+    if message.text == 'Picnic' or message.text == 'пикник':
         bot.send_message(message.chat.id, assortiment.picnic)
         answer = assortiment.picnic
         log(message, answer)
-    elif message.text == 'snickers' or message.text == 'сникерс':
-        bot.send_message(message.chat.id, assortiment.snickers)
-        answer = assortiment.snickers
+    elif message.text == 'Rafaello' or message.text == 'рафаелло':
+        bot.send_message(message.chat.id, assortiment.rafaello)
+        answer = assortiment.rafaello
         log(message, answer)
-    elif message.text == 'bounty':
-        bot.send_message(message.chat.id, assortiment.bounty)
-        answer = assortiment.bounty
+    elif message.text == 'Alpen Gold Funduk':
+        bot.send_message(message.chat.id, assortiment.apgfd)
+        answer = assortiment.apgfd
         log(message, answer)
-    elif message.text == 'Tofee':
-        bot.send_message(message.chat.id, assortiment.tofee)
-        answer = assortiment.tofee
+    elif message.text == 'Dark':
+        bot.send_message(message.chat.id, assortiment.dark)
+        answer = assortiment.dark
+        log(message, answer)
+    elif message.text == 'Alpen Gold':
+        bot.send_message(message.chat.id, assortiment.apgd)
+        answer = assortiment.apgd
+        log(message, answer)
+    elif message.text == 'KitKat':
+        bot.send_message(message.chat.id, assortiment.kitkat)
+        answer = assortiment.kitkat
         log(message, answer)
     elif message.text == 'заказ':
         bot.send_message(message.chat.id, 'Мы свяжемся с Вами в кратчайшие сроки')
         zakazchik = ('id = ' + str(message.from_user.id) + '\nИмя: ' + str(message.from_user.first_name))
         time.sleep(15)
-        bot.send_message(config.chat_id, 'Новый заказ!\n' + str(zakazchik) + ' ' + str(message.text))
-        answer = 'Мы свяжемся с Вами в кратчайшие сроки'
+        bot.send_message(config.pasha, 'Новый заказ!\n' + str(zakazchik))
+        answer = 'Мы свяжемся с Вами в кратчайшие сроки для уточнения заказа'
         log(message,answer)
     else:
         bot.send_message (message.chat.id, 'Я Вас не понимаю, извините')
---
+++ config.py
@@ -0,0 +1,3 @@
+chat_id = '308096338'
+token = '500912221:AAElyFmZEVdKyQTqlfMQpa7ayJI1PSqgUIA'
+pasha = '382598456'
